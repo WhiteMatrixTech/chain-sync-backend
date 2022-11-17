d7c5d929e7ecdb086995623b2ad9dcb1648cdecd
reimia <reimia@qq.com>
2022-11-17 16:36:10 +0800

    s3 local config

--- matrix-cloud-blockchain-syncer/src/main/java/com/matrix/blockchain/module/S3BeanModule.java
+++ matrix-cloud-blockchain-syncer/src/main/java/com/matrix/blockchain/module/S3BeanModule.java
@@ -1,5 +1,6 @@
 package com.matrix.blockchain.module;
 
+import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.transfer.TransferManager;
@@ -15,13 +16,21 @@ import org.springframework.context.annotation.Primary;
 @Configuration
 public class S3BeanModule {
 
+  @Value("${spring.profiles.active:local}")
+  private String profile;
+
   @Value("${cloud.aws.region.static}")
   private String region;
 
   @Bean
   @Primary
   public AmazonS3 getS3Client() {
-    return AmazonS3ClientBuilder.standard().withRegion(region).build();
+    if (!"local".equals(profile)) {
+      return AmazonS3ClientBuilder.standard().withRegion(region).build();
+    } else {
+      return AmazonS3ClientBuilder.standard().withEndpointConfiguration(
+          new AwsClientBuilder.EndpointConfiguration("http://localhost:9999", region)).build();
+    }
   }
 
   @Bean
